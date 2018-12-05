698b4b7e22e7ffa1fa1aae30a5f9b11b8b441d01
Florian Friedl <you@example.com>
2018-12-05 11:56:34 +0100

    03_Friedl_Solution

--- app.ts
+++ app.ts
@@ -3,41 +3,44 @@ import * as loki from 'lokijs';
 import * as express from 'express';
 import * as basic from 'express-basic-auth';
 
-var app = express();
-app.use(express.json());
+let server = express();
+server.use(express.json());
 
-const adminFilter = basic({ users: { admin: 'P@ssw0rd!' }});
+const port = 8000;
 
-const db = new loki(__dirname + '/db.dat', {autosave: true, autoload: true});
-let guests = db.getCollection('guests');
-if (!guests) {
-  guests = db.addCollection('guests');
-}
+let title = "Weihnachtsfeier";
+let location = "Naarn im Machlande";
+let date = new Date(2018, 11, 25);
 
-app.get('/guests', adminFilter, (req, res) => {
-  res.send(guests.find());
-});
-
-app.get('/party', (req, res, next) => {
-  res.send({
-    title: 'Happy new year!',
-    location: 'At my home',
-    date: new Date(2017, 0, 1)
+/*localhost:8000/party*/ 
+server.get("/party", function(request, recieve){
+  recieve.send({
+    title: title,
+    location: location,
+    date: date
   });
 });
 
-app.post('/register', (req, res, next) => {
-  if (!req.body.firstName || !req.body.lastName) {
-    res.status(BAD_REQUEST).send('Missing mandatory member(s)');
-  } else {
-    const count = guests.count();
-    if (count < 10) {
-      const newDoc = guests.insert({firstName: req.body.firstName, lastName: req.body.lastName});
-      res.status(CREATED).send(newDoc);
-    } else {
-      res.status(UNAUTHORIZED).send('Sorry, max. number of guests already reached');
+let db = new loki("loki.json");
+let guest = db.addCollection("guests");
+
+server.get("/guests", function(request, recieve){
+  recieve.send(guest.find());
+});
+
+server.post("/register", function(request, recieve){
+  if(request.body.firstName || request.body.lastName){
+    if(guest.count() >= 10){
+      recieve.status(UNAUTHORIZED).send("Maximum amount of guests is already here");
+    }else{
+      const newDoc = guest.insert({firstName: request.body.firstname, lastName: request.body.lastName});
+      recieve.status(CREATED).send("Guest succesfully created");
     }
+  }else{
+    recieve.status(BAD_REQUEST).send("Please insert your first- and lastname");
   }
 });
 
-app.listen(8080, () => console.log('API is listening'));
+server.listen(port, function(){
+  console.log("API is listening on port ", [port])
+});
